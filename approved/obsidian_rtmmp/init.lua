ebe41e89bb29f802da53d88c73976cf1d9eee5e9
Victor Hackeridze <hackeridze@gmail.com>
2012-03-14 00:46:34 +0600

    obsidian fix

--- approved/obsidian_rtmmp/init.lua
+++ approved/obsidian_rtmmp/init.lua
@@ -1,9 +1,9 @@
 -- Obsidian mod, originally made by <???>
 -- modifed by rtmmp-team
 
-minetest.register_node("obsidian-rtmmp:obsidian_block", {
-    tile_images = {"obsidian-rtmmp_block.png"},
-    inventory_image = minetest.inventorycube("obsidian-rtmmp_block.png"),
+minetest.register_node("obsidian_rtmmp:obsidian_block", {
+    tile_images = {"obsidian_rtmmp_block.png"},
+    inventory_image = minetest.inventorycube("obsidian_rtmmp_block.png"),
     is_ground_content = true,
     material = minetest.digprop_stonelike(15.0), -- obsidian is hard, like a stone, but harder
 })
@@ -24,6 +24,6 @@ minetest.register_abm({
     interval = 1,
     chance = 1,
     action = function(pos, node)
-        minetest.env:add_node(pos, {name="obsidian-rtmmp:obsidian_block"})
+        minetest.env:add_node(pos, {name="obsidian_rtmmp:obsidian_block"})
     end
 })
