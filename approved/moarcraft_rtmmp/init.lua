25d76e8ef7001fbfe758bf5bf559b26dcb445869
Victor Hackeridze <hackeridze@gmail.com>
2012-03-14 14:41:30 +0600

    moarcraft approved

---
+++ approved/moarcraft_rtmmp/init.lua
@@ -0,0 +1,17 @@
+minetest.register_craft({
+	output = 'node "moarcraft_rtmmp:stone_brick" 4',
+	recipe = {
+		{'node "default:stone"','node "default:stone"'},
+		{'node "default:stone"','node "default:stone"'},
+	}
+})
+
+minetest.register_node("moarcraft_rtmmp:stone_brick", {
+	tile_images = {"default_stone.png^moarcraft_rtmmp_stone_brick.png"},
+	inventory_image = minetest.inventorycube("default_stone.png^moarcraft_rtmmp_stone_brick.png"),
+	paramtype = "mineral",
+	is_ground_content = true,
+	often_contains_mineral = true, -- Texture atlas hint
+	material = minetest.digprop_stonelike(1.7),
+	dug_item = 'node "moarcraft_rtmmp:stone_brick" 1',
+})
