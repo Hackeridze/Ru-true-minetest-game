7ffe32b24f28241a91dc6f3a07d3eb2c57c979f5
Victor Hackeridze <hackeridze@gmail.com>
2012-03-14 17:05:58 +0600

    Dye's craft items added

--- shit/dye/init.lua
+++ shit/dye/init.lua
@@ -20,3 +20,85 @@ DYE_COLORS = {
 	"pink",
 	"brown",
 }
+
+
+-- Dyes
+minetest.register_craftitem("dye:white", {
+	description = "White dye",
+	inventory_image = "white_dye.png",
+})
+
+minetest.register_craftitem("dye:light_gray", {
+	description = "Light gray dye",
+	inventory_image = "light_gray_dye.png",
+})
+
+minetest.register_craftitem("dye:gray", {
+	description = "Gray dye",
+	inventory_image = "gray_dye.png",
+})
+
+minetest.register_craftitem("dye:black", {
+	description = "Black dye",
+	inventory_image = "black_dye.png",
+})
+
+minetest.register_craftitem("dye:red", {
+	description = "Red dye",
+	inventory_image = "red_dye.png",
+})
+
+minetest.register_craftitem("dye:orange", {
+	description = "Orange dye",
+	inventory_image = "orange_dye.png",
+})
+
+minetest.register_craftitem("dye:yellow", {
+	description = "Yellow dye",
+	inventory_image = "yellow_dye.png",
+})
+
+minetest.register_craftitem("dye:lime", {
+	description = "Lime dye",
+	inventory_image = "lime_dye.png",
+})
+
+minetest.register_craftitem("dye:green", {
+	description = "Green dye",
+	inventory_image = "green_dye.png",
+})
+
+minetest.register_craftitem("dye:light_blue", {
+	description = "Light blue dye",
+	inventory_image = "light_blue_dye.png",
+})
+
+minetest.register_craftitem("dye:cyan", {
+	description = "Cyan dye",
+	inventory_image = "cyan_dye.png",
+})
+
+minetest.register_craftitem("dye:blue", {
+	description = "Blue dye",
+	inventory_image = "blue_dye.png",
+})
+
+minetest.register_craftitem("dye:purple", {
+	description = "Purple dye",
+	inventory_image = "purple_dye.png",
+})
+
+minetest.register_craftitem("dye:magenta", {
+	description = "Magenta dye",
+	inventory_image = "magenta_dye.png",
+})
+
+minetest.register_craftitem("dye:pink", {
+	description = "Pink dye",
+	inventory_image = "pink_dye.png",
+})
+
+minetest.register_craftitem("dye:brown", {
+	description = "Brown dye",
+	inventory_image = "brown_dye.png",
+})
