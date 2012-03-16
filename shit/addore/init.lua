15a51d0032001e6756e2c1736a1538c5b98747fa
Ilya Zhuravlev <zhuravlevilya@ya.ru>
2012-03-16 14:22:55 +0400

    addore refactored, tested, got some new abilities

--- shit/addore/init.lua
+++ shit/addore/init.lua
@@ -1,85 +1,86 @@
+addore = function(oreName, oreDescription, oreTexture, oreMaterial,
+                  digTools, 
+                  maxAmountForChunk, rarity, size,
+                  minY, maxY,
+                  dropName, dropDescription, dropTexture, dropAmount)
 
-
-addore = function(oreName,oreDescription,
-				oreTexture,oreMaterial,
-				maxAmountForChunk,
-				rarity,size,
-				minY,maxY,
-				dropShitName,dropShitDescription,
-				dropShitTexture,dropShitAmount)
-
-local ore = "addore:" .. oreName
-local shit = "addore:" .. dropShitName
-
-if dropShit == -1 then
-	minetest.register_node(ore, {
-		description = oreDescription,
-		tile_images = {oreTexture},
-		is_ground_content = true,
-		material = orematerial,
-	})
-else      
-	minetest.register_craftitem(shit, {
-		description = dropShitDescription,
-		image = dropShitTexture,
-		on_place_on_ground = minetest.craftitem_place_item,
-	}) 
-	minetest.register_node(ore, {
-		description = oreDescription,
-		tile_images = {oreTexture},
-		is_ground_content = true,
-		material = orematerial,
-		drop = {
-			max_items = dropShitAmount,
-			items = {
-				{
-					items = {shit},
-					rarity = 1,
-				}
-			}
-		},
-	})
+    local ore = "addore:" .. oreName
+    local oreNode = {
+        description = oreDescription,
+        tile_images = {"default_stone.png^"..oreTexture},
+        is_ground_content = true,
+        material = oreMaterial
+    }
+    if dropName ~= nil then
+        local drop = ""
+        if dropName ~= "" then
+            drop = "addore:" .. dropName
+            minetest.register_craftitem(drop, {
+                description = dropDescription,
+                image = dropTexture
+            })
+        end
+        oreNode["drop"] = {
+            max_items = 1,
+            items = {
+                {
+                    items = {drop.." "..dropAmount},
+                    tools = digTools
+                }
+            }
+        }
+    end
+    minetest.register_node(ore, oreNode)
+    local gen = function(minp,maxp)
+        if minp.y < minY or maxp.y > maxY or math.random(1, rarity) ~= 1 then 
+            return 
+        end
+        
+        for a = 0, maxAmountForChunk do
+            local pos = {
+                x = math.random( minp.x, maxp.x ),
+                y = math.random( minp.y, maxp.y ),
+                z = math.random( minp.z, maxp.z ),
+            }
+            for i = -1, size.x - 1 do
+                for j = -1, size.y - 1 do
+                    for k = -1, size.z - 1 do
+                        if math.random(1, 5) > 2 then
+                        else
+                            local p = {x = pos.x + i, y = pos.y + j, z = pos.z + k}
+                            local n = minetest.env:get_node(p)
+                            if n.name == "default:stone" then
+                                minetest.env:add_node(p, {name = ore})
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+    minetest.register_on_generated(gen)
 end
 
-local gen = function(minp,maxp)
-    if math.random(1,rarity) ~= 1 then return; end
 
-	if  minp.y < minY or maxp.y > maxY then return; end
-    
-	for a = 0, maxAmountForChunk do
-		local pos = {
-			x = math.random( minp.x, maxp.x ),
-			y = math.random( minp.y, maxp.y ),
-			z = math.random( minp.z, maxp.z ),
-		}
-		for i = -1, size.x -1 do
-			for j = -1, size.y-1 do
-				for k = -1, size.z-1 do
-					if math.random(1,5) > 2 then
-					else
-						local p = { x=pos.x+i, y=pos.y+j, z=pos.z+k }
-						local n = minetest.env:get_node( p )
-						if n.name == "default:stone" then
-							minetest.env:add_node( p, { name = ore } )
-						end
-					end
-				end
-			end
-		end
-	end
-end
-minetest.register_on_generated(gen)
-end
-
+--[[addore("check_ore","BAD ORE",
+                "check_ore.png",minetest.digprop_stonelike(1.4),
+                50,
+                1,{
+                    x = 1,
+                    y = 1,
+                    z = 1,
+                },
+                -31000,100,
+                "huita","This is huita",
+                "check_shit.png",10, {"~pick"})]]
 
 addore("check_ore","BAD ORE",
-				"check_ore.png",minetest.digprop_stonelike(1.4),
-				50,
-				1,{
-					x = 1,
-					y = 1,
-					z = 1,
-				},
-				-31000,100,
-				"huita","This is huita",
-				"check_shit.png",10)
+                "check_ore.png",minetest.digprop_stonelike(1.4),
+                nil, -- any tool
+                50,
+                1,{
+                    x = 1,
+                    y = 1,
+                    z = 1,
+                },
+                -31000,100)
