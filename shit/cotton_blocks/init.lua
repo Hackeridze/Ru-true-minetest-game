7c1f9c47fb807e06640150e34ce781ec01cb9348
Victor Hackeridze <hackeridze@gmail.com>
2012-03-18 20:09:00 +0600

    all bad

--- shit/cotton_blocks/init.lua
+++ shit/cotton_blocks/init.lua
@@ -1,29 +1,30 @@
 
 local CB_NAMES_COLORS = {
-    "white" = "White cotton block",
-    "light_gray" = "Light-gray cotton block",
-    "gray" = "Gray cotton block",
-    "black" = "Black cotton block",
-    "red" = "Red cotton block",
-    "orange" = "Orange cotton block",
-    "yellow" = "Yellow cotton block",
-    "lime" = "Lime cotton block",
-    "green" = "Green cotton block",
-    "light_blue" = "Light-blue cotton block",
-    "cyan" = "Cyan cotton block",
-    "blue" = "Blue cotton block",
-    "purple" = "Purple cotton block",
-    "magenta" = "Magenta cotton block",
-    "pink" = "Pink cotton block",
-    "brown" = "Brown cotton block",
+    ["white"] = "White cotton block",
+    ["light_gray"] = "Light-gray cotton block",
+    ["gray"] = "Gray cotton block",
+    ["black"] = "Black cotton block",
+    ["red"] = "Red cotton block",
+    ["orange"] = "Orange cotton block",
+    ["yellow"] = "Yellow cotton block",
+    ["lime"] = "Lime cotton block",
+    ["green"] = "Green cotton block",
+    ["light_blue"] = "Light-blue cotton block",
+    ["cyan"] = "Cyan cotton block",
+    ["blue"] = "Blue cotton block",
+    ["purple"] = "Purple cotton block",
+    ["magenta"] = "Magenta cotton block",
+    ["pink"] = "Pink cotton block",
+    ["brown"] = "Brown cotton block",
 }
-
+print("OLOLOLOLOLOLOL XXXXXXXXXXXX\n")
 for color, name in ipairs(CB_NAMES_COLORS) do
+    print("cotton_blocks:" .. color .. "\n")
     minetest.register_node("cotton_blocks:" .. color, {
         description = name,
         tile_images = {"cb_" .. color .. ".png"},
         inventory_image = minetest.inventorycube("cb_" .. color .. ".png"),
         is_ground_content = true,
-        material = minetest.digprop_stonelike(0.3), -- WTF???????? FUCK!!!!!!!!!1111111!!!!!!!!!111
+        minetest.digprop_leaveslike(2.0), -- WTF???????? FUCK!!!!!!!!!1111111!!!!!!!!!111
     })
 end
