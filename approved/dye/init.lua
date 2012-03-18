576d1b7aea073940053992bd309c4dcd1347a583
Victor Hackeridze <hackeridze@gmail.com>
2012-03-18 18:45:31 +0600

    Tabs fix, cotton blocks started, obsidian description added

--- approved/dye/init.lua
+++ approved/dye/init.lua
@@ -3,187 +3,187 @@
 
 --Public colors for mods:
 DYE_COLORS = {
-	"white",
-	"light_gray",
-	"gray",
-	"black",
-	"red",
-	"orange",
-	"yellow",
-	"lime",
-	"green",
-	"light_blue",
-	"cyan",
-	"blue",
-	"purple",
-	"magenta",
-	"pink",
-	"brown",
+    "white",
+    "light_gray",
+    "gray",
+    "black",
+    "red",
+    "orange",
+    "yellow",
+    "lime",
+    "green",
+    "light_blue",
+    "cyan",
+    "blue",
+    "purple",
+    "magenta",
+    "pink",
+    "brown",
 }
 
 
 -- Dyes
 minetest.register_craftitem("dye:white", {
-	description = "White dye",
-	inventory_image = "dye_white.png",
+    description = "White dye",
+    inventory_image = "dye_white.png",
 })
 
 minetest.register_craftitem("dye:light_gray", {
-	description = "Light gray dye",
-	inventory_image = "dye_light_gray.png",
+    description = "Light gray dye",
+    inventory_image = "dye_light_gray.png",
 })
 
 minetest.register_craftitem("dye:gray", {
-	description = "Gray dye",
-	inventory_image = "dye_gray.png",
+    description = "Gray dye",
+    inventory_image = "dye_gray.png",
 })
 
 minetest.register_craftitem("dye:black", {
-	description = "Black dye",
-	inventory_image = "dye_black.png",
+    description = "Black dye",
+    inventory_image = "dye_black.png",
 })
 
 minetest.register_craftitem("dye:red", {
-	description = "Red dye",
-	inventory_image = "dye_red.png",
+    description = "Red dye",
+    inventory_image = "dye_red.png",
 })
 
 minetest.register_craftitem("dye:orange", {
-	description = "Orange dye",
-	inventory_image = "dye_orange.png",
+    description = "Orange dye",
+    inventory_image = "dye_orange.png",
 })
 
 minetest.register_craftitem("dye:yellow", {
-	description = "Yellow dye",
-	inventory_image = "dye_yellow.png",
+    description = "Yellow dye",
+    inventory_image = "dye_yellow.png",
 })
 
 minetest.register_craftitem("dye:lime", {
-	description = "Lime dye",
-	inventory_image = "dye_lime.png",
+    description = "Lime dye",
+    inventory_image = "dye_lime.png",
 })
 
 minetest.register_craftitem("dye:green", {
-	description = "Green dye",
-	inventory_image = "dye_green.png",
+    description = "Green dye",
+    inventory_image = "dye_green.png",
 })
 
 minetest.register_craftitem("dye:light_blue", {
-	description = "Light blue dye",
-	inventory_image = "dye_light_blue.png",
+    description = "Light blue dye",
+    inventory_image = "dye_light_blue.png",
 })
 
 minetest.register_craftitem("dye:cyan", {
-	description = "Cyan dye",
-	inventory_image = "dye_cyan.png",
+    description = "Cyan dye",
+    inventory_image = "dye_cyan.png",
 })
 minetest.register_alias("dye:blue", "lazurite:lazurite")
 --[[minetest.register_craftitem("dye:blue", {
-	description = "Blue dye",
-	inventory_image = "dye_blue.png",
+    description = "Blue dye",
+    inventory_image = "dye_blue.png",
 })]]
 
 minetest.register_craftitem("dye:purple", {
-	description = "Purple dye",
-	inventory_image = "dye_purple.png",
+    description = "Purple dye",
+    inventory_image = "dye_purple.png",
 })
 
 minetest.register_craftitem("dye:magenta", {
-	description = "Magenta dye",
-	inventory_image = "dye_magenta.png",
+    description = "Magenta dye",
+    inventory_image = "dye_magenta.png",
 })
 
 minetest.register_craftitem("dye:pink", {
-	description = "Pink dye",
-	inventory_image = "dye_pink.png",
+    description = "Pink dye",
+    inventory_image = "dye_pink.png",
 })
 
 minetest.register_craftitem("dye:brown", {
-	description = "Brown dye",
-	inventory_image = "dye_brown.png",
+    description = "Brown dye",
+    inventory_image = "dye_brown.png",
 })
 
 
 --Dye recipes
 local addSMrecipe = function(new, first, second) 
-	minetest.register_craft({
-		output = "dye:" .. new .." 2",
-		recipe = {
-				{"dye:".. first, "dye:" .. second},
-		}
-	})
-	
-	minetest.register_craft({
-		output = "dye:" .. new .." 2",
-		recipe = {
-				{"dye:".. second, "dye:" .. first},
-		}
-	})
-	
-	minetest.register_craft({
-		output = "dye:" .. new .." 2",
-		recipe = {
-				{"dye:".. first},
-				{"dye:".. second},
-		}
-	})
-	
-	minetest.register_craft({
-		output = "dye:" .. new .." 2",
-		recipe = {
-				{"dye:".. second},
-				{"dye:".. first},
-		}
-	})
-		
-	minetest.register_craft({
-		output = "dye:" .. new .." 2",
-		recipe = {
-				{"dye:".. second,""},
-				{"","dye:".. first},
-		}
-	})
-		
-	minetest.register_craft({
-		output = "dye:" .. new .." 2",
-		recipe = {
-				{"dye:".. first,""},
-				{"","dye:".. second},
-		}
-	})
-	
-	minetest.register_craft({
-		output = "dye:" .. new .." 2",
-		recipe = {
-				{"","dye:".. second},
-				{"dye:".. first,""},
-		}
-	})	
-	
-	minetest.register_craft({
-		output = "dye:" .. new .." 2",
-		recipe = {
-				{"","dye:".. first},
-				{"dye:".. second,""},
-		}
-	})
+    minetest.register_craft({
+        output = "dye:" .. new .." 2",
+        recipe = {
+                {"dye:".. first, "dye:" .. second},
+        }
+    })
+
+    minetest.register_craft({
+        output = "dye:" .. new .." 2",
+        recipe = {
+                {"dye:".. second, "dye:" .. first},
+        }
+    })
+
+    minetest.register_craft({
+        output = "dye:" .. new .." 2",
+        recipe = {
+                {"dye:".. first},
+                {"dye:".. second},
+        }
+    })
+
+    minetest.register_craft({
+        output = "dye:" .. new .." 2",
+        recipe = {
+                {"dye:".. second},
+                {"dye:".. first},
+        }
+    })
+
+    minetest.register_craft({
+        output = "dye:" .. new .." 2",
+        recipe = {
+                {"dye:".. second,""},
+                {"","dye:".. first},
+        }
+    })
+
+    minetest.register_craft({
+        output = "dye:" .. new .." 2",
+        recipe = {
+                {"dye:".. first,""},
+                {"","dye:".. second},
+        }
+    })
+
+    minetest.register_craft({
+        output = "dye:" .. new .." 2",
+        recipe = {
+                {"","dye:".. second},
+                {"dye:".. first,""},
+        }
+    })
+
+    minetest.register_craft({
+        output = "dye:" .. new .." 2",
+        recipe = {
+                {"","dye:".. first},
+                {"dye:".. second,""},
+        }
+    })
 end
 
 --second
 addSMrecipe("orange","red","yellow")
 minetest.register_craft({
-	output = "dye:orange 1",
-	recipe = {
-			{"flowers:flower_tulip"},
-	}
+    output = "dye:orange 1",
+    recipe = {
+            {"flowers:flower_tulip"},
+    }
 })
 addSMrecipe("cyan","green","blue")
 addSMrecipe("purple","red","blue")
 minetest.register_craft({
-	output = "dye:purple 1",
-	recipe = {
-			{"flowers:flower_viola"},
-	}
+    output = "dye:purple 1",
+    recipe = {
+            {"flowers:flower_viola"},
+    }
 })
 addSMrecipe("gray","black","white")
 addSMrecipe("light_blue","white","blue")
@@ -197,24 +197,24 @@ addSMrecipe("light_gray","gray","white")
 --first
 minetest.register_craft({
     type = "shapeless",
-	output = "dye:red 1",
-	recipe = {
-			"flowers:flower_rose",
-	}
+    output = "dye:red 1",
+    recipe = {
+            "flowers:flower_rose",
+    }
 })
 minetest.register_craft({
     type = "shapeless",
-	output = "dye:white 1",
-	recipe = {
-			"flowers:flower_dandelion_white",
-	}
+    output = "dye:white 1",
+    recipe = {
+            "flowers:flower_dandelion_white",
+    }
 })
 minetest.register_craft({
     type = "shapeless",
-	output = "dye:yellow 1",
-	recipe = {
-			"flowers:flower_dandelion_yellow",
-	}
+    output = "dye:yellow 1",
+    recipe = {
+            "flowers:flower_dandelion_yellow",
+    }
 })
 minetest.register_craft({
     type = "shapeless",
@@ -225,17 +225,17 @@ minetest.register_craft({
 })
 minetest.register_craft({
     type = "shapeless",
-	output = "dye:brown 2", -- because bric is big
-	recipe = {
-			"default:clay_brick",
-	}
+    output = "dye:brown 2", -- because bric is big
+    recipe = {
+            "default:clay_brick",
+    }
 })
 minetest.register_craft({
     type = "shapeless",
-	output = "dye:black 1",
-	recipe = {
-			"default:scorched_stuff",
-	}
+    output = "dye:black 1",
+    recipe = {
+            "default:scorched_stuff",
+    }
 })
 
 
---
+++ shit/cotton_blocks/init.lua
@@ -0,0 +1,19 @@
+
+local CB_NAMES_COLORS = {
+    "white" = "White cotton block",
+    "light_gray" = "Light-gray cotton block",
+    "gray" = "Gray cotton block",
+    "black" = "Black cotton block",
+    "red" = "Red cotton block",
+    "orange" = "Orange cotton block",
+    "yellow" = "Yellow cotton block",
+    "lime" = "Lime cotton block",
+    "green" = "Green cotton block",
+    "light_blue" = "Light-blue cotton block",
+    "cyan" = "Cyan cotton block",
+    "blue" = "Blue cotton block",
+    "purple" = "Purple cotton block",
+    "magenta" = "Magenta cotton block",
+    "pink" = "Pink cotton block",
+    "brown" = "Brown cotton block",
+}
