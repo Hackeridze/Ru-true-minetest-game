e8e5dacb45dd4f8b1b40c59a70cc5845ac3b3ee6
Victor Hackeridze <hackeridze@gmail.com>
2012-03-19 15:44:01 +0600

    Lazurite block added

--- approved/lazurite/init.lua
+++ approved/lazurite/init.lua
@@ -9,3 +9,19 @@ addore("lazurite","lazurite_ore","Lazurite ore",
                 },
                 -31000,-300,
                 "lazurite", "Lazurite", "dye_blue.png", 4)
+                
+minetest.register_node("lazurite:lazurite_block", {
+    description = "Block of lazurite",
+    tile_images = {"lazurite_block.png"},
+    is_ground_content = true,
+    material = minetest.digprop_stonelike(1.2),
+})
+
+minetest.register_craft({
+    output = 'lazurite:lazurite_block',
+    recipe = {
+        { 'lazurite:lazurite','lazurite:lazurite','lazurite:lazurite'},
+        { 'lazurite:lazurite','lazurite:lazurite','lazurite:lazurite'},
+        { 'lazurite:lazurite','lazurite:lazurite','lazurite:lazurite'},
+    }
+})
