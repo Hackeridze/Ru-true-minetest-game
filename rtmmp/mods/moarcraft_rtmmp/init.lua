bdb96e11e7bd83054d100165ef4a626146f173de
Victor Hackeridze <hackeridze@gmail.com>
2012-04-04 16:29:39 +0600

    fixes stone brick

--- rtmmp/mods/moarcraft_rtmmp/init.lua
+++ rtmmp/mods/moarcraft_rtmmp/init.lua
@@ -8,7 +8,6 @@ minetest.register_craft({
 
 minetest.register_node("moarcraft_rtmmp:stone_brick", {
 	tile_images = {"default_stone.png^moarcraft_rtmmp_stone_brick.png"},
-	inventory_image = minetest.inventorycube("default_stone.png^moarcraft_rtmmp_stone_brick.png"),
 	paramtype = "mineral",
 	is_ground_content = true,
 	often_contains_mineral = true, -- Texture atlas hint
