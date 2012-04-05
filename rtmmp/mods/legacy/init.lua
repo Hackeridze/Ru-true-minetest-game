a523c5554d6b8af6820721ef85781cda4f4787d8
Victor Hackeridze <hackeridze@gmail.com>
2012-04-05 15:28:09 +0600

    legacy 128-stack

--- rtmmp/mods/legacy/init.lua
+++ rtmmp/mods/legacy/init.lua
@@ -78,6 +78,7 @@ minetest.register_alias("scorched_stuff", "default:scorched_stuff")
 minetest.register_craftitem(":rat", {
 	description = "Rat",
 	inventory_image = "rat.png",
+	stack_max = 128,
 	on_drop = function(item, dropper, pos)
 		minetest.env:add_rat(pos)
 		item:take_item()
@@ -97,11 +98,13 @@ minetest.register_craftitem(":cooked_rat", {
 	description = "Cooked rat",
 	inventory_image = "cooked_rat.png",
 	on_use = minetest.item_eat(6),
+	stack_max = 128,
 })
 
 minetest.register_craftitem(":firefly", {
 	description = "Firefly",
 	inventory_image = "firefly.png",
+	stack_max = 128,
 	on_drop = function(item, dropper, pos)
 		minetest.env:add_firefly(pos)
 		item:take_item()
